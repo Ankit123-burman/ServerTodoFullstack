e64b0541d9f9a6ce165c218dc9ae7d97cc645f60
priyanshuburman540@gmail.com <151107409+Ankit123-burman@users.noreply.github.com>
2024-12-24 21:36:08 +0530

    url

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors({
     origin: process.env.URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE',"OPTIONS"],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 mongoose
