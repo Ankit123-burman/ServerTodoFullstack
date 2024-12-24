d1fe05b3680e8dc9220d419f5770b94ab40dd63e
priyanshuburman540@gmail.com <151107409+Ankit123-burman@users.noreply.github.com>
2024-12-24 21:19:50 +0530

    url

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT | 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: process.env.URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE',"OPTIONS"],
+    credentials: true,
+}));
 
 mongoose
     .connect(process.env.MONGO_DB)
